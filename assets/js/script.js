ad024cd0ab87a2c64994d2acb72bad234b6836e0
lukaszglowacz <>
2023-07-10 13:07:52 +0000

    Add function to preload enter website image before other elements start to loading.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,6 @@
-console.log("Let's begin this site")
+// Start enter image animation, when website is fully loaded
+
+window.addEventListener('load', function() {
+    // After the page is fully loaded, make the body visible
+    document.body.classList.add('visible');
+});
